8604faa771be584e8825a6f339150617480ff934
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-02-02 11:20:42 -0300

    projeto 01 da cadeira

---
+++ projeto01/Ambiente.h
@@ -0,0 +1,10 @@
+#ifndef AMBIENTE_H
+#define AMBIENTE_H
+
+class Ambiente {
+    public:
+        int x,y;
+        int estacao_carregamento_x,estacao_carregamento_y;
+}
+
+#endif
---
+++ projeto01/main.cpp
@@ -0,0 +1,13 @@
+//cd C:\Users\eggui '.\Documents\02estudo\programacao_avancada\projeto01'
+//g++ main.cpp Ambiente.cpp -o aula39_poo
+//./main 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
