06b14e7dcb9e468d1d098b793f3099650778f1b0
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-02-26 15:27:26 -0300

    completei o exercicio

--- exercicio_04/exercicio4.cpp
+++ exercicio_04/exercicio4.cpp
@@ -10,12 +10,20 @@ class Node{
         Node* p_NodeNext;   //aponta para o proximo no
 
         //construtor
-        Node(float a, float b){
-        x=a;
-        y=b;
-        cout<<"No criado"<<endl;
-        cout<<"Valor de x: "<<x<<" Valor de Y: "<<y<<endl;
+        Node(){
+
         };
+        Node(float a, float b){
+            x=a;
+            y=b;
+            cout<<"No criado"<<endl;
+            cout<<"Valor de x: "<<x<<" Valor de Y: "<<y<<endl;
+        };
+
+        void printNode(){
+            cout<<"O valor do no"<<endl;
+            cout<<"Valor de x: "<<x<<" Valor de Y: "<<y<<endl;
+        }
 };
 
 class Lista{
@@ -35,16 +43,56 @@ class Lista{
         }
         //printa a lista
         void pirntListNode(){
+            Node* inicio = head;    //Salvar o inicio
             while(head != NULL){
                 cout<< "Para esse no x= "<<head->x<<" e Y= "<<head->y <<endl;
                 head = head->p_NodeNext;
             }
+            head = inicio;          //retornar para o inicio
+        }
+
+        //procurar no
+        Node* procuraNode(Node nodeProcurado){
+            cout<<"Vou procurar o no"<<endl;
+            Node* inicio = head;    //salvar o inicio
+            Node* retorno;
+            while(head != NULL){
+                //cout<<"Entrei no loop"<<endl;
+                if(head->x==nodeProcurado.x&&head->y==nodeProcurado.y){
+                    cout<<"No encontrado"<<endl;
+                    //cout<<"Valor de x: "<<head->x <<" Valor de Y: "<< head->y<<endl;
+                    retorno= head;
+                }
+                head = head->p_NodeNext;
+            }
+            head = inicio;          //retornar para o inicio
+            return retorno;
+        }
+
+        //excluir no
+        void excluirNode(Node nodeExcluido){
+            Node* inicio = head;
+            Node* Anterior = head;
+
+            while(head!=NULL){
+                if(head->x==nodeExcluido.x&&head->y==nodeExcluido.y){
+                    cout<<"No encontrado, vai ser excluido"<<endl;
+                    Anterior->p_NodeNext=head->p_NodeNext;
+                    // o no anterior aponta para o sucessor do no atual
+                }
+                Anterior = head; 
+                head = head->p_NodeNext;
+            }
+            head = inicio;
         }
 };
 
 int main(){
     //inicinaod a lista
     Lista lista1;
+
+    //alocando memoria para o No
+    Node* busca;
 
     //iniciando o no
     Node primeiroNo(10.0,12.5);
@@ -62,6 +110,12 @@ int main(){
     lista1.insereNodeNaLista(quartoNo);
 
     cout<<"printando a lista"<<endl;
+    lista1.pirntListNode();
+
+    busca = lista1.procuraNode(primeiroNo);
+    busca->printNode();
+
+    lista1.excluirNode(segundoNo);
     lista1.pirntListNode();
 
     return 0;
